d2b9c8cfd219f25922d5dc917380810a661aaa3c
Emily Goeres <goeres123@hotmail.com>
2020-09-24 19:51:29 -0600

    Fixing github saves

--- script.js
+++ script.js
@@ -1,84 +1,97 @@
+
+
+
+var cityArr = [];
 var APIKey = "3562aea0ee193ab4d5e31803854c69bb";
-var cityName="Seattle";
-  function saveCity() {
-      cityName=$('#cityName').val();
-      $("#listCityName").append("<li onclick='weatherInfo(this)'>"+cityName+"</li>");
-      $('#cityName').val("");
-      weatherInfo(cityName);
-      fiveDayWeatherInfo(cityName);
-  }
-    // Created and API Key
- 
+var cityName = "Seattle";
+var cityFromLocal =JSON.parse (localStorage.getItem("city"))||[]
+for (var i=0; i<cityFromLocal.length; i++) {
+  $("#listCityName").append("<li onclick='weatherInfo(this)'>" + cityFromLocal[i] + "</li>");
+}
+// We need to check with an if statement to see if there was somthing saved to our cityFromLocal var if(citiFromLocal !== null), then using a for loop we need to add all the cities in cityFromlocal to our cityArr, we need to create the li's for cities and append them to the page( something likd )  $("#listCityName").append("<li onclick='weatherInfo(this)'>" + cityName + "</li>");
+  $('#cityName').val("");
+function saveCity() {
+  cityName = $('#cityName').val();
+  $("#listCityName").append("<li onclick='weatherInfo(this)'>" + cityName + "</li>");
+  $('#cityName').val("");
+  weatherInfo(cityName);
+  fiveDayWeatherInfo(cityName);
+  cityFromLocal.push(cityName)
+  localStorage.setItem("city",JSON.stringify(cityFromLocal))
+}
+
+// 
 
 
 function weatherInfo(c) {
-    cityName=c.innerHTML || c;
+  cityName = c.innerHTML || c;
 
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-      "q="+cityName+"&appid=" + APIKey;
+  // Here we are building the URL we need to query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=" + cityName + "&appid=" + APIKey;
 
-    // created an AJAX 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function(response) {
+  // created an AJAX 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
 
-        // Log the queryURL
-        console.log(queryURL);
+      // Log the queryURL
+      console.log(queryURL);
 
-        // Log the resulting object
-        console.log(response);
-        var lat=response.coord.lat
-        var lon = response.coord.lon
-       
+      // Log the resulting object
+      console.log(response);
+      var lat = response.coord.lat
+      var lon = response.coord.lon
 
-        // Transfer content to HTML
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        $(".clouds").text("Description: " + response.weather[0].main);
-        $("#cloudyIcon").attr("src",`https://openweathermap.org/img/wn/${response.weather[0].icon}.png`);
 
-        // Convert the temp to fahrenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+      // Transfer content to HTML
+      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
+      $(".clouds").text("Description: " + response.weather[0].main);
+      $("#cloudyIcon").attr("src", `https://openweathermap.org/img/wn/${response.weather[0].icon}.png`);
 
-        // add temp content to html
-        $(".temp").text("Temperature (K) " + response.main.temp);
-        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+      // Convert the temp to fahrenheit
+      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
-        UVInfo(lat,lon)
-      });
-    }
+      // add temp content to html
+      $(".temp").text("Temperature (K) " + response.main.temp);
+      $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-    function fiveDayWeatherInfo(cityName) {
-    
-        // Here we are building the URL we need to query the database
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" +"q="+cityName+"&appid=" + APIKey+"&units=imperial";
+      // Log the data in the console as well
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (F): " + tempF);
+      UVInfo(lat, lon)
+    });
+}
 
-        // created an AJAX 
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-          // We store all of the retrieved data inside of an object called "response"
-          .then(function(response) {
-    
-            // Log the queryURL
-            console.log(queryURL);
+function fiveDayWeatherInfo(cityName) {
 
-    
-            // Log the resulting object
-            console.log(response); 
-            var htmlString=""
-            for (var i=0; i<response.list.length; i=i+8) {
-                htmlString +=`
+  // Here we are building the URL we need to query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + "q=" + cityName + "&appid=" + APIKey + "&units=imperial";
+
+
+  // created an AJAX 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+
+      // Log the resulting object
+      console.log(response);
+      var htmlString = ""
+      for (var i = 0; i < response.list.length; i = i + 8) {
+        htmlString += `
                 <div class="card">
                     <h6>${response.list[i].dt_txt}</h6>
                     <p>Temp: ${response.list[i].main.temp}<p>
@@ -87,36 +100,36 @@ function weatherInfo(c) {
                     <img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png"></img>
                 </div>
                 `
-            }
-            console.log(htmlString)
-            $("#container").append(htmlString)
-    
-        
-          });
-        }
-        function UVInfo(lat,lon) {
-            
-            // Here we are building the URL we need to query the database
-            var queryURL =   `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
-            // created an AJAX 
-            $.ajax({
-              url: queryURL,
-              method: "GET"
-            })
-              // We store all of the retrieved data inside of an object called "response"
-              .then(function(response) {
-        
-                // Log the queryURL
-                console.log(response);
-                $("#UVInfo").append("UVInfo"+response.value)
-              })
-            }
-            
+      }
+      console.log(htmlString)
+      $("#container").append(htmlString)
+
+
+    });
+}
+function UVInfo(lat, lon) {
+
+  // Here we are building the URL we need to query the database
+  var queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
+  // created an AJAX 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+      // Log the queryURL
+      console.log(response);
+      $("#UVInfo").append("UVInfo" + response.value)
+    })
+}
+
 // Need to store this in local storage
-            $(".saveBtn").on("click", function () {
-                var id = $(this).attr("id")
-                var time = id.split("-")[1]
-                var i = $("#txt-" + time).val()
-                console.log(id, time, "txt-" + time, i)
-                localStorage.setItem(time, i)
-            })
+// $(".saveBtn").on("click", function () {
+//   var id = $(this).attr("id")
+//   var time = id.split("-")[1]
+//   var i = $("#txt-" + time).val()
+//   console.log(id, time, "txt-" + time, i)
+//   localStorage.setItem(city, i)
+// })
